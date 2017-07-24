8ff94ae02c0d998609f303cc5833c95e82b5296f
thinkerpiash <masterpiash@gmail.com>
2017-07-24 22:21:49 +0600

    first php assignment

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$a = 80;
+$b = 90;
+$c = 50;
+  
+  
+if ($a > $b && $a > $c) {
+  
+echo'A is Big';
+}
+  else if ($b > $a && $b > $c ) {
+    echo 'B is the big one';
+} 
+else if ($c > $a && $c > $b) {
+    echo 'C Big';
+}
+
